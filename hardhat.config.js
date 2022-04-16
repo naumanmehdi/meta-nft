51bc909e21cd39df8905158a5953f8db6040b69f
naumanmehdi <QWtj4etSRNPW2f3>
2022-04-16 16:01:55 +0500

    Final Testnet fix and contract deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,14 +4,15 @@
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 const { API_URL, PRIVATE_KEY } = process.env;
+console.log(API_URL);
 module.exports = {
    solidity: "0.8.0",
    defaultNetwork: "rinkeby",
    networks: {
       hardhat: {},
-      ropsten: {
-         url: API_URL,
-         accounts: [`0x${PRIVATE_KEY}`]
+      rinkeby: {
+        url: API_URL,
+        accounts: [`0x${PRIVATE_KEY}`]
       }
    },
 }
